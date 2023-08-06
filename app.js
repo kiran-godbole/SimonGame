d2baab2752ba690ac1d7b2f1f363ad4fcd019be0
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-08-06 19:36:50 +0530

    eventlistner trigged if any kry press

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const gameSeq = []
 const userSeq = []
 
-const started = false;
+let started = false;
 const level = 0;
 
 document.addEventListener("keypress", function(){
-    console.log("game started");
-})
+    if(started == false){
+        console.log("game started");
+        started = true;
+    }
+});
+
